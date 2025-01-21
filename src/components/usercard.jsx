47999a183f38d4f75d4bda37f153793be4a3cbce
unknown <sera.shine@kalvium.community>
2025-01-21 10:32:11 +0530

    design_a_component

--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -2,15 +2,15 @@
 import React from "react";
 
 const Usercard=()=>{
-    const profilePhoto="";
-    const name="Sera Shine";
-    const email="sera@gmail.com"
-    const phoneNumber="1234568790"
-    const address="123 Main Street"
+    const profilePhoto = "https://upload.wikimedia.org/wikipedia/commons/a/a5/European_Rabbit%2C_Lake_District%2C_UK_-_August_2011.jpg";
+    const name="Sera Shine"
+    const email="sera@gmail.com";
+    const phoneNumber="1234568790";
+    const address="123 Main Street";
 
     return(
         <div>
-        <img src={profilePhoto}/>
+        <img src={profilePhoto} style={{width:"100px",height:"100px"}}/>
         <h2>{name}</h2>
         <p>{email}</p>
         <p>{phoneNumber}</p>
